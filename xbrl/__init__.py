a1a244561fb2dc4044d2edeb4d932f7568751e45
Manuel Schmidt <manu.schmidt.14@gmail.com>
2025-02-11 19:22:26 +0100

    Updated package to V2.2.14

--- xbrl/__init__.py
+++ xbrl/__init__.py
@@ -76,7 +76,7 @@ class ContextParseException(InstanceParseException):
     pass
 
 
-__version__ = '2.2.13'
+__version__ = '2.2.14'
 __author__ = 'Manuel Schmidt <hello@schmidt-manuel.de>'
 __all__ = [
     XbrlParseException,
