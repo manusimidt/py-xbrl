2e4ce80c290f397eba250b049294d6f2e7f4915c
Manuel Schmidt <manu.schmidt.14@gmail.com>
2025-10-12 21:45:59 +0200

    Updated package to V2.2.15

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="py-xbrl",
-    version="2.2.14",
+    version="2.2.15",
     url="https://github.com/manusimidt/xbrl_parser",
     license='GNU General Public License v3 (GPLv3)',
     author="Manuel Schmidt",
--- xbrl/__init__.py
+++ xbrl/__init__.py
@@ -76,7 +76,7 @@ class ContextParseException(InstanceParseException):
     pass
 
 
-__version__ = '2.2.14'
+__version__ = '2.2.15'
 __author__ = 'Manuel Schmidt <hello@schmidt-manuel.de>'
 __all__ = [
     XbrlParseException,
