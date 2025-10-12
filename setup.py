12333b4655f546095a86d151aa508458cb2d3b17
Manuel Schmidt <manu.schmidt.14@gmail.com>
2025-10-12 21:52:47 +0200

    Updating setuptools

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
